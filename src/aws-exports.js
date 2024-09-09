e9cbd2779d47d7dbe7100a0a92336867683a28f8
root <root@ip-172-31-12-185.ap-south-1.compute.internal>
2024-09-09 07:59:34 +0000

    srcexprots

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,9 +3,8 @@
 
 const awsmobile = {
     "aws_project_region": "ap-south-1",
-    "https://jxvv6zjm2bd7dlvefjghsgxtle.appsync-api.ap-south-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://jxvv6zjm2bd7dlvefjghsgxtle.appsync-api.ap-south-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-south-1",
     "aws_appsync_authenticationType": "API_KEY",
     "aws_appsync_apiKey": "da2-3eihd23kzzegtdl63w5ipaw64a"
 };
-
